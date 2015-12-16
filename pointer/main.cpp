a468034d461fa27c36e72e80f687e6dbbd41d7c6
gmreason <453948208@qq.com>
2015-12-16 19:12:19 +0800

    Initial Commit

---
+++ pointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pointer
+//
+//  Created by gmr on 15/12/16.
+//  Copyright © 2015年 gmr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
