103aac003cb0e1fb48fe4dfceebbb0def220befb
gmreason <453948208@qq.com>
2015-12-16 19:16:39 +0800

    pointer
    输出整形变量a指针变量p的地址

--- pointer/main.cpp
+++ pointer/main.cpp
@@ -11,5 +11,10 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    int a,*p;
+    a=100;
+    p=&a;
+    printf("a=%x &a=%x p=%x &p=%x *p=%x\n",a,&a,p,&p,*p);
     return 0;
 }
